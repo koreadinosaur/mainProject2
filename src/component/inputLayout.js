642961b216d5056e4303752cb54e8437fce9c797
HyeongJin Kim <khj7741@gmail.com>
2022-11-01 20:50:01 +0900

    회원가입 페이지 input 레이아웃

--- src/component/inputLayout.js
+++ src/component/inputLayout.js
@@ -2,15 +2,19 @@ import React from "react";
 import styled from "styled-components";
 const Label = styled.label`
   display: inline-block;
-  width: 5rem;
+  width: 10rem;
 `;
 
 const ElementContainer = styled.div`
   display: inline-block;
   margin-left: 0.7rem;
+  flex: 1 1 auto;
 `;
 const LayoutContainer = styled.div`
+  width: 45rem;
   margin-bottom: 1.5rem;
+  font-size: 1.6rem;
+  display: flex;
 `;
 
 const InputLayout = ({ children, label }) => {
