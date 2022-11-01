f8017924eb8f19eaace6119a4d6e1eba08fe0c3b
HyeongJin Kim <khj7741@gmail.com>
2022-11-01 14:30:37 +0900

    라벨, input,버튼 레이아웃

--- src/component/inputLayout.js
+++ src/component/inputLayout.js
@@ -1,12 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+const Label = styled.label`
+  display: inline-block;
+  width: 5rem;
+`;
+
+const ElementContainer = styled.div`
+  display: inline-block;
+  margin-left: 0.7rem;
+`;
+const LayoutContainer = styled.div`
+  margin-bottom: 1.5rem;
+`;
 
 const InputLayout = ({ children, label }) => {
+  /* children이 배열의 경우 어떻게 처리하나? 은수님께 여쭤볼 것. */
   return (
-    <div>
-      <label>{label || "라벨"}</label>
-      <input />
-      {children}
-    </div>
+    <LayoutContainer>
+      <Label>{label || "라벨"}</Label>
+      {Array.isArray(children) ? (
+        children.map((item, index) => {
+          return <ElementContainer key={index}>{item}</ElementContainer>;
+        })
+      ) : (
+        <ElementContainer>{children}</ElementContainer>
+      )}
+    </LayoutContainer>
   );
 };
 
