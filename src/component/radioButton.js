18c599b4e85253f67730e2355242f143a76d79fd
HyeongJin Kim <khj7741@gmail.com>
2022-11-05 20:26:38 +0900

    태그에 속성값 동적으로 추가하는 코드 리팩터링

--- src/component/radioButton.js
+++ src/component/radioButton.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 const Input = styled.input`
   margin-left: 1rem;
 `;
 
-const RadioButton = ({ value, name, user }) => {
+const RadioButton = ({ value, name, user, register, label }) => {
+  const [genderChecked, setGenderChecked] = useState();
+
+  const handleClick = (e) => {
+    setGenderChecked("checked");
+  };
+
   return (
     <>
-      <label>{value || "라벨"}</label>
-      {user === value ? (
-        <Input type="radio" name={name} value={value} checked></Input>
-      ) : (
-        <Input type="radio" name={name} value={value}></Input>
-      )}
+      <label>
+        {value || "라벨"}
+        <Input
+          type="radio"
+          {...register(label, { required: false })}
+          name={name}
+          value={value}
+          onClick={handleClick}
+          genderChecked
+          {...(user === value ? { defaultChecked: true } : {})}
+        ></Input>
+      </label>
     </>
   );
 };
