2b7a8b106c055240d60a3e4f87832b627827f14f
HyeongJin Kim <khj7741@gmail.com>
2022-11-08 22:32:04 +0900

    next Js - Hello world

---
+++ next_signup/components/layout/MainNavigation.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const MainNavigation = () => {
+  return (
+    <nav>
+      <Link href="/login">login</Link>
+    </nav>
+  );
+};
+
+export default MainNavigation;
---
+++ next_signup/pages/login/index.js
@@ -0,0 +1,4 @@
+function Login() {
+  return <div>hi this is Login page</div>;
+}
+export default Login;
