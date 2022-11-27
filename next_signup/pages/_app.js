62636f70834fc5668238fe184aa901f39292b3aa
HyeongJin Kim <khj7741@gmail.com>
2022-11-27 21:40:26 +0900

    redux Provider wrapper 추가, 더미데이터 삭제

--- next_signup/pages/_app.js
+++ next_signup/pages/_app.js
@@ -1,50 +1,17 @@
 import { useState } from "react";
+import { Provider } from "react-redux";
+import { store } from "../src/store";
+import { useSelector } from "react-redux";
 import PageLayout from "../components/layout/PageLayout";
 import "../styles/globals.css";
-const data = [
-  {
-    uuid: 1,
-    username: "human_001",
-    password: 1234,
-    dateOfBirth: { year: "1500", month: "07", date: "07" },
-    gender: "female",
-    email: "human_001@gmail.com",
-    phone: ["82", "010", "0000", "0000"],
-    introduction: "ㅎㅇ",
-  },
-  {
-    uuid: 2,
-    username: "human_002",
-    password: 1234,
-    dateOfBirth: { year: "1500", month: "07", date: "07" },
-    gender: "female",
-    email: "human_001@gmail.com",
-    phone: ["82", "010", "0000", "0000"],
-    introduction: "ㅎㅇ",
-  },
-  {
-    uuid: 3,
-    username: "human_003",
-    password: 1234,
-    dateOfBirth: { year: "1500", month: "07", date: "07" },
-    gender: "female",
-    email: "human_001@gmail.com",
-    phone: ["82", "010", "0000", "0000"],
-    introduction: "ㅎㅇ",
-  },
-];
+
 function MyApp({ Component, pageProps }) {
-  const [users, setUsers] = useState(data);
-  const [currentUser, setCurrentUser] = useState();
   return (
-    <PageLayout>
-      <Component
-        {...pageProps}
-        users={users}
-        currentUser={currentUser}
-        setCurrentUser={setCurrentUser}
-      />
-    </PageLayout>
+    <Provider store={store}>
+      <PageLayout>
+        <Component {...pageProps} />
+      </PageLayout>
+    </Provider>
   );
 }
 
