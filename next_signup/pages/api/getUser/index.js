64fc90f7845483bd7b8cd04a1207fd97370f9f28
HyeongJin Kim <khj7741@gmail.com>
2022-11-29 22:24:08 +0900

    mongoDB find 메서드 findOne으로 변경

--- next_signup/pages/api/getUser/index.js
+++ next_signup/pages/api/getUser/index.js
@@ -1,5 +1,8 @@
+import { verifyPassword } from "../../../lib/auth";
+
 const { MongoClient } = require("mongodb");
 async function handler(req, res) {
+  const { password, username } = req.body;
   const url = `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PASSWORD}@cluster0.qzj5nsv.mongodb.net/?retryWrites=true&w=majority`;
   const client = new MongoClient(url);
 
@@ -7,21 +10,21 @@ async function handler(req, res) {
     await client.connect();
     const db = client.db("users");
     const usersCollections = db.collection("users");
-
-    const find = await usersCollections.find().toArray();
+    const isExistingUser = await usersCollections.findOne({
+      username: username,
+    });
     // if (req.method === "GET") {
     //   console.log(req);
     //   console.log(req.params);
     //   return res.send("hi");
     // }
-    const findUser = find.filter(
-      (user) =>
-        user.username === req.body.username &&
-        user.password === req.body.password
-    );
-    console.log(findUser[0]);
+    if (!isExistingUser)
+      return res.status(422).send({ message: "존재하지 않는 유저입니다." });
+    const isValid = await verifyPassword(password, isExistingUser.password);
+    if (!isValid)
+      return res.status(422).send({ message: "비밀번호가 틀렸습니다" });
 
-    return res.json(findUser[0]);
+    return res.json(isExistingUser);
   } catch (err) {
     console.log(err.stack);
   } finally {
