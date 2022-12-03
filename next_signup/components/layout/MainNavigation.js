319eb2d3dc4ab678588fa6b063c5bc023d3d61c5
HyeongJin Kim <khj7741@gmail.com>
2022-12-03 17:31:05 +0900

    useSession에 따라 navbar 리스트 변경

--- next_signup/components/layout/MainNavigation.js
+++ next_signup/components/layout/MainNavigation.js
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { Fragment } from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
+import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 const NavLink = styled(Link)`
   margin-right: 1rem;
 
@@ -10,16 +12,30 @@ const NavLink = styled(Link)`
     color: gray;
   }
 `;
+const Logout = styled.button`
+  background: none;
+  border: none;
+  font-size: inherit;
+  color: black;
+  &:hover {
+    border-bottom: 3px solid red;
+    color: gray;
+    cursor: pointer;
+  }
+`;
 
 const MainNavigation = () => {
+  const { data: session, status } = useSession();
+  console.log(session, status);
   const currentUser = useSelector((state) => state.user.value);
   return (
     <Fragment>
       <NavLink href="/">Home</NavLink>
-      <NavLink href="/login">login</NavLink>
-      <NavLink href="/signup">signup</NavLink>
-      {currentUser.username ? <NavLink href="/mypage">mypage</NavLink> : null}
+      {!session ? <NavLink href="/login">login</NavLink> : null}
+      {!session ? <NavLink href="/signup">signup</NavLink> : null}
+      {session ? <NavLink href="/mypage">mypage</NavLink> : null}
       <NavLink href="/users">Users</NavLink>
+      {session ? <Logout onClick={() => signOut()}>logout</Logout> : null}
     </Fragment>
   );
 };
