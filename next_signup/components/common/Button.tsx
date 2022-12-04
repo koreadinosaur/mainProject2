233dc5e24683867dd5974ba88188fd864cd81350
HyeongJin Kim <khj7741@gmail.com>
2022-12-04 16:46:03 +0900

    typescript 리팩터링

---
+++ next_signup/components/common/Button.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+const ButtonElement = styled.button`
+  display: inline-block;
+  border: none;
+  border-radius: 0.3rem;
+  background-color: ${(props) => props.bgColor};
+  height: 3rem;
+  color: white;
+  width: ${(props) => props.btnWidth};
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+interface ButtonProps {
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void; // to handle onClick functions
+  children?: React.ReactNode; // make the component able to receive children elements
+  bgColor?: string;
+  width?: string;
+  type?: string;
+  errors?: string;
+  buttonName?: string;
+}
+const Button = ({
+  buttonName,
+  bgColor,
+  width,
+  type,
+  onClick,
+  errors,
+}: ButtonProps) => {
+  return (
+    <>
+      <ButtonElement
+        type={type || null}
+        bgColor={bgColor || "skyblue"}
+        btnWidth={width}
+        onClick={onClick}
+        {...(errors ? { disabled: true } : {})}
+      >
+        {buttonName || "제출"}
+      </ButtonElement>
+    </>
+  );
+};
+
+export default Button;
---
+++ next_signup/components/common/DropDown.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+const Select = styled.select`
+  height: 3rem;
+`;
+
+interface DropDownProps {
+  options: string[];
+  name: string;
+  register: (label: string, option: {}) => void;
+  user: string;
+  label: string;
+  required: string;
+}
+
+const DropDown = ({
+  options,
+  name,
+  user,
+  register,
+  label,
+  required,
+}: DropDownProps) => {
+  return (
+    <>
+      <Select
+        name={name}
+        {...register(label, { required })}
+        defaultValue={user}
+      >
+        {name === "site" ? (
+          <option key="a0" value="직접입력">
+            직접입력
+          </option>
+        ) : null}
+        {options &&
+          options.map((item, index) => (
+            <option key={index} value={item}>
+              {item}
+            </option>
+          ))}
+      </Select>
+    </>
+  );
+};
+
+export default DropDown;
