714b4c1b19c807e7199023222487d57780fc0aab
HyeongJin Kim <khj7741@gmail.com>
2022-12-10 12:44:48 +0900

    material UI 설정

--- next_signup/pages/_app.js
+++ next_signup/pages/_app.js
@@ -1,18 +1,23 @@
-import { useState } from "react";
 import { Provider } from "react-redux";
 import { store } from "../src/store";
-import { useSelector } from "react-redux";
 import PageLayout from "../components/layout/PageLayout";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
-
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "styled-components";
+const theme = {
+  primary: "green",
+};
+function MyApp({ Component, pageProps: { session }, pageProps }) {
   return (
     <SessionProvider session={session}>
       <Provider store={store}>
-        <PageLayout>
-          <Component {...pageProps} />
-        </PageLayout>
+        <CssBaseline />
+        <ThemeProvider theme={theme}>
+          <PageLayout>
+            <Component {...pageProps} />
+          </PageLayout>
+        </ThemeProvider>
       </Provider>
     </SessionProvider>
   );
